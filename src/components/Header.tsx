b50644e7d119f7a1c13dbd7db945876e77c43419
Julian Celeita <andresceleita@yahoo.es>
2023-04-05 16:14:59 -0500

    Header temporally finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,6 @@ function Header() {
             className=" p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
           />
           <MagnifyingGlassIcon className="h-12 p-4" />
-          <Bars4Icon className="h-12 p-4" />
         </div>
 
         <div className="text-white flex justify-between items-center text-xs space-x-6 mx-6 whitespace-nowrap">
@@ -39,11 +38,31 @@ function Header() {
             <p className="font-extrabold md:text-sm">y Ordenes</p>
           </div>
           <div className="relative flex link items-center">
-            <span className="absolute top-0 right-0 md:right-11 h-4 w-4 bg-yellow-400 text-center rounded-full font-bold text-black">0</span>
+            <span className="absolute top-0 right-0 md:right-11 h-4 w-4 bg-amber-600 text-center rounded-full font-bold text-black">
+              0
+            </span>
             <ShoppingCartIcon className="h-10" />
-            <p className="hidden md:inline font-extrabold md:text-sm">Carrito</p>
+            <p className="hidden md:inline font-extrabold md:text-sm">
+              Carrito
+            </p>
           </div>
         </div>
+      </div>
+
+      <div className="flex items-center bg-store_blue-light space-x-3 p-2 pl-6 text-sm text-white">
+        <p className="flex link items-center">
+          <Bars4Icon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden md:inline-flex">Electronics</p>
+        <p className="link hidden md:inline-flex">Food & Grocery</p>
+        <p className="link hidden md:inline-flex">Prime</p>
+        <p className="link hidden md:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
     </header>
   );
