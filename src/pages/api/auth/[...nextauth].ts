d5ceaacdf9cef0cb6f5322a108d7868a560638ab
Julian Celeita <andresceleita@yahoo.es>
2023-04-22 18:19:16 -0500

    deleted sign in with Facebook

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,12 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-
-
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-    /* TODO add login with facebook */
-    FacebookProvider({
-        clientId: process.env.GOOGLE_ID as string,
-        clientSecret: process.env.GOOGLE_SECRET as string,
-      }),
+    })
   ],
 }
 export default NextAuth(authOptions)
